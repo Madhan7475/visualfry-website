91b6728704a19630d4d2b4927dc0e655c2217f0c
Madhan7475 <visualfry@gmail.com>
2026-09-16 12:57:55 +0530

    color change

--- visualfry-studio-react/src/components/Navbar.jsx
+++ visualfry-studio-react/src/components/Navbar.jsx
@@ -4,8 +4,18 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => setIsOpen(!isOpen);
-  const closeMenu = () => setIsOpen(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+    if (!isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  };
+  const closeMenu = () => {
+    setIsOpen(false);
+    document.body.style.overflow = '';
+  };
 
   return (
     <header>
